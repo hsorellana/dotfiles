abd370d1613084e482c2b0e80ed6105a86078ab8
Hugo Soto <hsotoorellana@gmail.com>
2021-02-20 22:58:37 -0300

    Change brewfile for shell script

---
+++ setup/brew.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+brew tap homebrew/bundle
+brew tap homebrew/cask
+brew tap warrensbox/tap
+
+brew install coreutils
+brew install go
+brew install detox
+brew install fzf
+brew install gh
+brew install git
+brew install git-crypt
+brew install graphviz
+brew install htop
+brew install hugo
+brew install jq
+brew install protobuf
+brew install warrensbox/tap/tfswitch
+brew install terraform-docs
+brew install the_silver_searcher
+brew install tree
+brew install wget
+brew install youtube-dl
+brew install wifi-password
+brew install k9s
+brew install neovim
+brew install mas
+brew install httpie
+brew install speedtest-cli
+brew install terminal-notifier
+brew install golang-migrate
+
+
+brew cask install google-chrome
+brew cask install spotify
+brew cask install iterm2
+brew cask install slack
+brew cask install 1password
+brew cask install postman
+brew cask install zoom
+brew cask install virtualbox
+# brew cask install minikube
+brew cask install spectacle
+brew cask install vlc
+brew cask install tunnelblick
+brew cask install ngrok
+brew cask install sequel-pro
+brew cask install visual-studio-code
